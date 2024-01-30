ea2b0a295d0102d0cccb62082420f2ab84226cd4
atoms <atomsis01@mail.ru>
2024-01-30 21:01:12 +0300

    добавил систему коментариев и доделал форму "поделиться"

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,69 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Post
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
 from django.http import Http404
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from django.views.generic import  ListView
-from .forms import EmailPostForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
+
 
 # Create your views here.
 
-def post_share(request,post_id):
+@require_POST
+def post_comment(request, post_id):
     post = get_object_or_404(Post,
-                             id =post_id,
-                             status = Post.Status.PUBLISHED)
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post,
+                   'form': form,
+                   'sent': sent})
+
 
 def post_list(request):
     post_list = Post.published.all()
 
-    paginator = Paginator(post_list,2)
-    page_number = request.GET.get('page',1)
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -25,18 +72,23 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts':posts},)
+                  {'posts': posts}, )
 
-def post_detail(request,id):
-    post  = get_object_or_404(Post,
-                              status = Post.Status.PUBLISHED,
-                              slug=post,
-                              publish__year=year,
-                              publish__month=month,
-                              publish__day=day)
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post': post,
+                   'comments':comments,
+                   'form':form})
 
 # class PostListView(ListView):
 #     queryset =  Post.published.all()
