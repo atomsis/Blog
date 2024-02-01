35137cafff3a9ee85621430a5ec46c1902f80822
Atomsis <atomsis01@mail.ru>
2024-02-01 17:21:47 +0300

    Добавил markdown, feed представление и теги

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
-
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
@@ -59,8 +60,13 @@ def post_share(request, post_id):
                    'sent': sent})
 
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     paginator = Paginator(post_list, 2)
     page_number = request.GET.get('page', 1)
@@ -72,7 +78,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts}, )
+                  {'posts': posts,
+                   'tag': tag},)
 
 
 def post_detail(request, year, month, day, post):
@@ -84,11 +91,18 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments':comments,
-                   'form':form})
+                   'form':form,
+                   'similar_posts': similar_posts})
 
 # class PostListView(ListView):
 #     queryset =  Post.published.all()
